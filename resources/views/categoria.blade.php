11395636c4ada061a911f64695bad0fd7ad609da
Faherme46 <82012489+Faherme46@users.noreply.github.com>
2023-10-25 17:22:29 -0500

    añadiendo archivos

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -1,7 +1,14 @@
-
+<link rel="stylesheet" href="assets/css/categoria.css">
 @extends('layouts.app')
 
 @section('content')
+    <div class="content">
+        <h1>categoria</h1>
 
-    <h1>categoria</h1>
+
+
+
+
+    </div>
+
 @endsection
