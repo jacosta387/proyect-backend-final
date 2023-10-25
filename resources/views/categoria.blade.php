ac516c8c418d8b7246f07a652c39eb9ee12d79e6
Robinson1906 <robinsonleonmoreno213@gmail.com>
2023-10-25 17:26:03 -0500

    ingreso de categorias

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -3,3 +3,76 @@
 @section('content')
 
 <h1>categoria</h1>
+
+<section>
+    <div class="content">
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 1"  >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Matial peak.jpg" class="d-block w-100" alt="Imagen 2">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Matial peak.jpg" class="d-block w-100" alt="Imagen 3">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Matial peak.jpg" class="d-block w-100" alt="Imagen 4">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\img\Matial peak.jpg" class="d-block w-100" alt="Imagen 5" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 6">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 7" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 8">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 9">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 10">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 11">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 12">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Siguiente</span>
+            </button>
+        </div>
+
+</section>
+
+@endsection
