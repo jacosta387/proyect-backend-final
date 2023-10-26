bedb54bf5b96a885acd70f0cbe3a236d554e25f2
Faherme46 <82012489+Faherme46@users.noreply.github.com>
2023-10-26 00:51:50 -0500

    Detallitos

---
+++ resources/views/genero.blade.php
@@ -0,0 +1,234 @@
+<link rel="stylesheet" href="assets/css/genero.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+@extends('layouts.app')
+
+@section('content')
+    <div class="titulo">
+        <h1>categoria</h1>
+
+
+
+        <div class="titulo">
+            <h2>==========Misterio==========</h2>
+<section>
+    <div class="content">
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 1"  >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 2">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 3">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 4">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 5" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 6">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 7" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 8">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 9">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 10">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 11">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 12">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Siguiente</span>
+            </button>
+        </div>
+
+</section>
+
+<div class="titulo">
+    <h2>==========Accion==========</h2>
+
+<section>
+    <div class="content">
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 1"  >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 2">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 3">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 4">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 5" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 6">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 7" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 8">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 9">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 10">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 11">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 12">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Siguiente</span>
+            </button>
+        </div>
+
+</section>
+
+    <div class="titulo">
+        <h2>==========Aventura==========</h2>
+
+<section>
+    <div class="content">
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 1"  >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 2">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 3">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 4">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 5" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 6">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 7" >
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 8">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 9">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 10">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 11">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 12">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Siguiente</span>
+            </button>
+        </div>
+
+</section>
+@endsection
+
