40ed1a6c9f8593fbef695eb148d189d0fe5ea36d
Robinson1906 <robinsonleonmoreno213@gmail.com>
2023-10-26 11:14:14 -0500

    Terminado arreglo de genero

--- resources/views/genero.blade.php
+++ resources/views/genero.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/css/categoria.css">
+<link rel="stylesheet" href="assets/css/genero.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
 </script>
@@ -129,8 +129,7 @@
                                 <p class="card-text">Emilton lo mama</p>
                             </div>
                         </div>
-                    </div>
-                    <div class="press">
+                    </div> <div class="press">
                         <div class="card" style="width: 18rem;">
                             <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
