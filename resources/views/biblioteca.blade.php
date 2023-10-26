418e2fd4e2c0a67550fb6d40155c5e51a6ebe5d6
Robinson1906 <robinsonleonmoreno213@gmail.com>
2023-10-26 11:59:50 -0500

    Terminado maquetado de busqueda y agregado tanto a biblioteca como a busqueda las cards

--- resources/views/biblioteca.blade.php
+++ resources/views/biblioteca.blade.php
@@ -6,7 +6,47 @@
         <h2>Leidos Recientemente</h2>
         <hr class="hr" />
 
+        <div class="press-container">
+            <div class="press">
+                <div class="card" style="width: 18rem;">
+                    <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Emilton lo mama</p>
+                    </div>
+                </div>
+            </div>
+            <div class="press">
+                <div class="card" style="width: 18rem;">
+                    <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Emilton lo mama</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="titulo">
         <h2>Lista de Lectura</h2>
         <hr class="hr" />
+
+        <div class="press-container">
+            <div class="press">
+                <div class="card" style="width: 18rem;">
+                    <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Emilton lo mama</p>
+                    </div>
+                </div>
+            </div>
+            <div class="press">
+                <div class="card" style="width: 18rem;">
+                    <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Emilton lo mama</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    
 @endsection
