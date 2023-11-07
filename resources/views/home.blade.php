ff157df997d110eb833184188193d3bee974d64f
Faherme46 <82012489+Faherme46@users.noreply.github.com>
2023-11-07 17:45:56 -0500

    home y anadir

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,63 +2,68 @@
 
 @section('content')
     <link rel="stylesheet" href="assets/css/home.css">
+
     <div class="content">
+
+        <div class="titulo">
+            <h2>Favoritos del mes</h2>
+        </div>
 
         <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></button>
                 <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1"></button>
                 <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <div class="row">
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 1">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 2">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 3">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100" alt="Imagen 4">
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 5">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 6">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 7">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100" alt="Imagen 8">
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 9">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 10">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 11">
+                        <?php
 
-                        </div>
-                        <div class="col-md-3">
-                            <img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100" alt="Imagen 12">
-                        </div>
+                        for ($i=1; $i < 5; $i++) {
+                            # code...
+                            echo '<div class="col-md-3">';
+                                ?><a href="{{ route('manga') }}"><?php
+                        echo '<img src="..\public\assets\img\MartialPeak.jpg" class="d-block w-100 img-carousel"';
+                        echo '    alt="Imagen ' . $i . '">';
+                        ?></a><?php
+                            echo '</div>';
+                        }
+                            ?>
+
+                    </div>
+                </div>
+
+
+                <div class="carousel-item active">
+                    <div class="row">
+                        <?php
+
+                        for ($i=1; $i < 5; $i++) {
+                            # code...
+                            echo '<div class="col-md-3">';
+                                ?><a href="{{ route('manga') }}"><?php
+                        echo '<img src="..\public\assets\img\Apotheosis.jpg" class="d-block w-100 img-carousel"';
+                        echo '    alt="Imagen ' . $i . '">';
+                        ?></a><?php
+                            echo '</div>';
+                        }
+                            ?>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <?php
+                        for ($i=1; $i < 5; $i++) {
+                            # code...
+                            echo '<div class="col-md-3">';
+                                ?><a href="{{ route('manga') }}"><?php
+                        echo '<img src="..\public\assets\img\Yuan Zun.jpg" class="d-block w-100 img-carousel"';
+                        echo '    alt="Imagen ' . $i . '">';
+                        ?></a><?php
+                            echo '</div>';
+                        }
+                            ?>
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
@@ -71,74 +76,62 @@
                 </button>
             </div>
         </div>
-            <div class="press-container ">
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press ">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <p class="card-text">Descripcion de Manga</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
+        <div class="interesar    mt-3">
+            <div class="titulo">
+                <h2>Te puede interesar</h2>
+            </div>
+            <div class="press-container mr-3">
+
+                <?php
+                    for ($i=1; $i < 6; $i++) {
+                        # code...
+                        echo '<div class="press">';
+                            echo'<div class="card" style="width: 18rem;">';
+                                    ?><a href="{{ route('manga') }}"><?php
+                                        echo '<img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">';
+                                    ?></a><?php
+                                echo '<div class="card-body">';
+                                    echo '<p class="card-text">Descripcion de Manga</p>';
+                                echo '</div>';
+                            echo '</div>';
+                        echo'</div>';
+                        }
+                        ?>
+            </div>
         </div>
+        <div class="mas mt-3">
+            <div class="titulo">
+                <h2>Mas Mangas</h2>
+            </div>
+            <div class="press-container mr-3 ">
+
+                <?php
+                    for ($i=1; $i < 6; $i++) {
+                        # code...
+                        echo '<div class="press">';
+                            echo'<div class="card" style="width: 18rem;">';
+                                    ?><a href="{{ route('manga') }}"><?php
+                                        echo '<img src="..\public\assets\img\Apotheosis.jpg" class="card-img-top" alt="...">';
+                                    ?></a><?php
+                                echo '<div class="card-body">';
+                                    echo '<p class="card-text">Descripcion de Manga</p>';
+                                echo '</div>';
+                            echo '</div>';
+                        echo'</div>';
+                        }
+                        ?>
+            </div>
+        </div>
+
+
+    </div>
+
+    </div>
+
+
+
+    </div>
 
     </div>
 @endsection
