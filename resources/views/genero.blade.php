5f0c4edb260959713b8d49c26ce7c1698e498908
Faherme46 <82012489+Faherme46@users.noreply.github.com>
2023-11-07 19:09:55 -0500

    Fix problem with navbar

--- resources/views/genero.blade.php
+++ resources/views/genero.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
     <link rel="stylesheet" href="assets/css/genero.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+
     <div class="content">
         <div class="titulo">
             <h2>Misterio</h2>
@@ -146,4 +144,7 @@
         </div>
 
     </div>
+
+
 @endsection
+
