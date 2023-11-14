961f4f784e640953fc75fc6a130492a325697f62
jacosta387 <54636221+jacosta387@users.noreply.github.com>
2023-11-14 17:16:12 -0500

    revision controller Search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Manga
+use App\Models\Manga;
 
 class SearchController extends Controller
 {
     public function buscar(Request $request)
     {
         $query = $request->input('q');
-        $resultados = mangas::where('nombre', 'like', "%$query%")->get();
+        $resultados = manga::where('nombre', 'like', "%$query%")->get();
 
         return view('resultados_busqueda', compact('resultados'));
     }
