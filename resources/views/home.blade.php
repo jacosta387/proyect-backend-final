dc318afa42a1d8e8dc40920934f0e7a7cd97b65d
Faherme46 <82012489+Faherme46@users.noreply.github.com>
2023-11-14 11:10:14 -0500

    Carrusel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 @inject('mangaController', 'App\Http\Controllers\MangaController')
 @php
     $mangas = $mangaController->obtenerMangas();
+    $manga1 = $mangas[0];
 @endphp
 @section('content')
     <link rel="stylesheet" href="assets/css/home.css">
@@ -11,51 +12,101 @@
             <h2>Favoritos del mes</h2>
             <div id="carouselExampleIndicators" class="carousel slide">
                 <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                        aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
                 </div>
                 <div class="carousel-inner">
-                  <?php
-                  $mangasArray = $mangas->toArray();
-                  shuffle($mangasArray);
-                  
-                  $numMangas = min(12, count($mangasArray));
-                  
-                  for ($i = 0; $i < $numMangas; $i++) {
-                      $mangaAleatorio = $mangasArray[$i];
-                  ?>
-                  <div class="carousel-item <?php echo $i === 0 ? 'active' : ''; ?>">
-                    <div class="press">
-                      <div class="card" style="width: 18rem;">
-                        <a href="{{ route('manga', ['manga' => $mangaAleatorio['id_manga']]) }}">
-                          <img src="{{ $mangaAleatorio['portada'] }}" class="card-img-top" alt="...">
-                        </a>
-                      </div>
+                    <div class="carousel-item active  ">
+                        <div class="row">
+                            @for ($i = 0; $i < 4; $i++)
+                                @php
+                                    $mangaAleatorio = [];
+                                    for ($i = 0; $i < 5; $i++) {
+                                        $mangaAleatorio[] = $mangas[rand(1, count($mangas)-1)];
+                                    }
+                                @endphp
+                                @foreach ($mangaAleatorio as $manga)
+                                    <div class="press">
+                                        <div class="card" style="width: 18rem;">
+                                            <a href="{{ route('manga', ['manga' => $manga['id_manga']]) }}">
+                                                <img src="{{ $manga['portada'] }}" class="card-img-top" alt="...">
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endfor
+                        </div>
                     </div>
-                  </div>
-                  <?php
-                  }
-                  ?>
+                    <div class="carousel-item active ">
+                        <div class="row">
+                            @for ($i = 0; $i < 4; $i++)
+                                @php
+                                    $mangaAleatorio = [];
+                                    for ($i = 0; $i < 5; $i++) {
+                                        $mangaAleatorio[] = $mangas[rand(1, count($mangas)-1)];
+                                    }
+                                @endphp
+                                @foreach ($mangaAleatorio as $manga)
+                                    <div class="press">
+                                        <div class="card" style="width: 18rem;">
+                                            <a href="{{ route('manga', ['manga' => $manga['id_manga']]) }}">
+                                                <img src="{{ $manga['portada'] }}" class="card-img-top" alt="...">
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endfor
+                        </div>
+                    </div>
+                    <div class="carousel-item active ">
+                        <div class="row">
+                            @for ($i = 0; $i < 4; $i++)
+                                @php
+                                    $mangaAleatorio = [];
+                                    for ($i = 0; $i < 5; $i++) {
+                                        $mangaAleatorio[] = $mangas[rand(1, count($mangas)-1)];
+                                    }
+                                @endphp
+
+                                @foreach ($mangaAleatorio as $manga)
+                                    <div class="press">
+                                        <div class="card" style="width: 18rem;">
+                                            <a href="{{ route('manga', ['manga' => $manga['id_manga']]) }}">
+                                                <img src="{{ $manga['portada'] }}" class="card-img-top" alt="...">
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endfor
+                        </div>
+                    </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-        </div>
-
-
-        <div class="interesar   mt-3">
-            <div class="titulo">
-                <h2>Te puede interesar</h2>
             </div>
-            <div class="press-container">
-                <?php
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    </div>
+
+
+    <div class="interesar   mt-3">
+        <div class="titulo">
+            <h2>Te puede interesar</h2>
+        </div>
+        <div class="press-container">
+            <?php
                 // Convierte la colección a un array y luego reorganiza aleatoriamente
                 $mangasArray = $mangas->toArray();
                 shuffle($mangasArray);
@@ -66,47 +117,47 @@
                 for ($i = 0; $i < $numMangas; $i++) {
                     $mangaAleatorio = $mangasArray[$i];
                 ?>
-                <div class="press">
-                    <div class="card" style="width: 18rem;">
-                        <a href="{{ route('manga', ['manga' => $mangaAleatorio['id_manga']]) }}">
-                            <img src="{{ $mangaAleatorio['portada'] }}" class="card-img-top" alt="...">
-                        </a>
-                        <div class="card-body">
-                            <p class="card-text">{{ $mangaAleatorio['titulo'] }}</p>
-                        </div>
+            <div class="press">
+                <div class="card" style="width: 18rem;">
+                    <a href="{{ route('manga', ['manga' => $mangaAleatorio['id_manga']]) }}">
+                        <img src="{{ $mangaAleatorio['portada'] }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <p class="card-text">{{ $mangaAleatorio['titulo'] }}</p>
                     </div>
                 </div>
-                <?php
+            </div>
+            <?php
                 }
                 ?>
-            </div>
         </div>
+    </div>
 
-        <div class="mas mt-3">
-            <div class="titulo">
-                <h2>Mas Mangas</h2>
-            </div>
-            <div class="press-container">
-                @foreach ($mangas as $manga)
-                    {{-- <div>
+    <div class="mas mt-3">
+        <div class="titulo">
+            <h2>Mas Mangas</h2>
+        </div>
+        <div class="press-container">
+            @foreach ($mangas as $manga)
+                {{-- <div>
                         <h2>{{ $manga->titulo }}</h2>
                         <p> <img src={{ $manga->portada }} class="card-img-top" alt="...">';{{ $manga->descripcion }}</p>
 
                         <!-- Agrega más campos según sea necesario -->
                     </div> --}}
 
-                    <div class="press">
-                        <div class="card" style="width: 18rem;">
-                            <a href="{{ route('manga', ['manga' => $manga->id_manga]) }}">
-                                <img src="{{ $manga->portada }}" class="card-img-top" alt="...">
-                            </a>
-                            <div class="card-body">
-                                <p class="card-text">{{ $manga->titulo }}</p>
-                            </div>
+                <div class="press">
+                    <div class="card" style="width: 18rem;">
+                        <a href="{{ route('manga', ['manga' => $manga->id_manga]) }}">
+                            <img src="{{ $manga->portada }}" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <p class="card-text">{{ $manga->titulo }}</p>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
-    @endsection
+    </div>
+@endsection
